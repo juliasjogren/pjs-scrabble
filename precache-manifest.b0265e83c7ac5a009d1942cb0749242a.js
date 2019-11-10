2cbe7d9489890cfeef46a2dd6a3868f5f1636060
juliasjogren <juliaadelasjogren@gmail.com>
2019-11-10 17:19:52 +0100

    Updates

---
+++ precache-manifest.b0265e83c7ac5a009d1942cb0749242a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812144ea6560da3d8c0bf08887427adc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9e1126f183b430d6926",
+    "url": "/static/css/main.8c26615d.chunk.css"
+  },
+  {
+    "revision": "34d78662a9a5ec689c59",
+    "url": "/static/js/2.3580e45b.chunk.js"
+  },
+  {
+    "revision": "f9e1126f183b430d6926",
+    "url": "/static/js/main.ef6cb38b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
